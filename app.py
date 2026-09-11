5330876d3d97432fe47f0e319cf4f0d0a794efb9
Gian Barrantes <129077296+Gbarrantes25@users.noreply.github.com>
2026-09-10 22:20:41 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from pathlib import Path
 
 raiz = Path(__file__).resolve().parent
-dir_logo = (raiz / "src" /"assets" / "logo.svg").as_posix()
+dir_logo = (raiz / "src" / "assets" / "logo.svg").as_posix()
 dir_inicio = str(raiz / "src" / "pages" / "Inicio.py")
-dir_anual = str(raiz / "src" / "pages" / "Analisis_Anual.py")
+dir_anual = str(raiz / "src" / "pages" / "General.py")
 dir_ciudades = str(raiz / "src" / "pages" / "Estados_Ciudades.py")
+dir_detalles = str(raiz / "src" / "pages" / "Detalles.py")
 
 # Configurando página
 st.set_page_config(
@@ -18,15 +19,15 @@ st.set_page_config(
 # Enrutamiento
 logo = st.logo(image=dir_logo, size="large")
 inicio = st.Page(dir_inicio, title="Inicio", default=True)
-anual = st.Page(dir_anual, title="Análisis Anual")
+general = st.Page(dir_anual, title="General")
 ciudades = st.Page(dir_ciudades, title="Estados & Ciudades")
+detalles = st.Page(dir_detalles, title="Detalles")
 documentacion = st.Page("https://github.com/Gbarrantes25", title="Documentación")
 
 pg = st.navigation(
-    [inicio, anual, ciudades, documentacion],
+    [inicio, general, ciudades, detalles, documentacion],
     position="top",
 )
-
 
 
 pg.run()
